8b64e13b696373d4d8bdfae3a0ad858b1c92374a
sarajorheim <110483192+sarajorheim@users.noreply.github.com>
2022-11-17 12:09:31 +0100

    added Dictionary component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer className="App-footer">
+          <small>Coded by Sara Jörheim via SheCodes </small>
+        </footer>
+      </div>
     </div>
   );
 }
